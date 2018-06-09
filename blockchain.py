dbb359f1632dee6876c1a017168e7e9e8b8b0a5f
Albert Lam <albert233@hotmail.com>
2018-06-09 13:16:32 +1000

    Added node register and resolve endpoints in blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -2,7 +2,7 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 from urllib.parse import urlparse
 
@@ -209,7 +209,7 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    values = requests.get_json()
+    values = request.get_json()
 
     # Check that the required fields are in the POST'ed data
     required = ['sender', 'recipient', 'amount']
@@ -229,6 +229,42 @@ def full_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain),
     }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
     return jsonify(response), 200
 
 
